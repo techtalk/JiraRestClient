bb7808a56eded7ec2f6d92622a1859de992366b8
ToodF <fevzikaba@gmail.com>
2014-01-09 12:22:53 +0200

    added get issue types

---
+++ TechTalk.JiraRestClient/IssueType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechTalk.JiraRestClient
+{
+    public class IssueType
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public string iconUrl { get; set; }
+        public bool subtask { get; set; }
+    }
+}
