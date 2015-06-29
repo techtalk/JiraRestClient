ba15ecd593ced71eb77eab859ad900c9f8755677
Sebastian Sebusæter Enberget <sebastian.sebusaeter@gmail.com>
2015-06-29 20:05:39 +0200

    Added avatarurl

---
+++ TechTalk.JiraRestClient/AvatarUrls.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using RestSharp.Deserializers;
+
+namespace TechTalk.JiraRestClient
+{
+    public class AvatarUrls
+    {
+        [DeserializeAs(Name = "16x16")]
+        public string sixteen { get; set; }
+        [DeserializeAs(Name = "24x24")]
+        public string twentyfour { get; set; }
+        [DeserializeAs(Name = "32x32")]
+        public string thirtytwo { get; set; }
+        [DeserializeAs(Name = "48x48")]
+        public string fourteigth { get; set; }
+    }
+}
--- TechTalk.JiraRestClient/JiraUser.cs
+++ TechTalk.JiraRestClient/JiraUser.cs
@@ -4,9 +4,11 @@ namespace TechTalk.JiraRestClient
 {
     public class JiraUser
     {
+        public string self { get; set; }
         public string name { get; set; }
         public string emailAddress { get; set; }
         public string displayName { get; set; }
         public bool active { get; set; }
+        public AvatarUrls avatarUrls { get; set; }
     }
 }
