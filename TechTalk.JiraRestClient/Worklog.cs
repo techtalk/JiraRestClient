4481895e2ee9a39b4c12665ca436090844065e53
Daryayan <daryasa1991@mail.ru>
2022-03-16 20:27:34 +0300

    Adding properties for Worklog

--- TechTalk.JiraRestClient/Worklog.cs
+++ TechTalk.JiraRestClient/Worklog.cs
@@ -1,6 +1,16 @@
-﻿namespace TechTalk.JiraRestClient
+﻿using System;
+
+namespace TechTalk.JiraRestClient
 {
     public class Worklog
     {
+        public JiraUser author { get; set; }
+        public JiraUser updateAuthor { get; set; }
+        public string comment { get; set; }
+        public DateTime updated { get; set; }
+        public string timeSpent { get; set; }
+        public int timeSpentSeconds { get; set; }
+        public string id { get; set; }
+        public string issueId { get; set; }
     }
 }
